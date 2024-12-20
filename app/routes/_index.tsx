d74e7c64195857687dd3ac6123ff8660d4d897fb
bhaskarguthula <bhaskarguthula1674@gmail.com>
2024-12-20 12:25:46 +0530

    menu styles updated

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ const Index = () => {
   return (
     <div className='h-screen w-full p-20 flex flex-col gap-5'>
       <h2 className='text-3xl font-semibold'>Templates</h2>
-      <ul className='list-disc ps-10'>
+      <ul className='list-disc ps-10 flex flex-col gap-1'>
         <li className='hover:text-green-300 transition-colors duration-300'><Link to={'tabs'}>Tabs</Link></li>
       </ul>
     </div>
